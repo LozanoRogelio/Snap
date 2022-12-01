9c529684f2765a6e76a649d65685339e2bef350c
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-12-01 15:29:53 -0600

    Finished the main grid and footer. Will finish of mobile responsiveness another time.

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return (
+        <footer>
+            <p className="footer--p">Created by Rogelio Lozano - FrontendMentor Challeng</p>
+            
+        
+        </footer>
+    )
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,17 @@
+
+
 export default function Main() {
     return (
         <div className="grid">
-            <h1 className="main--h1">Make Remote work</h1>
-            <p className="main--p">Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch producitvity soar.</p>
-            <button className="main--button" type="button">Learn More</button>
-            <img src="/images/desktop.png" alt="guy with laptop" />
+            <div className="grid--content">
+            <h1 className="main--h1">Make <br/>Remote work</h1>
+            <p className="main--p">Get your team in sync, no matter your location. <br/>Streamline processes, create team rituals, and <br/>watch producitvity soar.</p>
+            <button className="main--button" type="button"><strong>Learn More</strong></button>
+            </div>
+            <div className="grid--image">
+            <img className="main--img" src="./images/desktop.png"  alt="guy with laptop" />
+            </div>
         </div>
+        
     )
 }
