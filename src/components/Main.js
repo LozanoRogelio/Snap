63f2dbf299439cf720b4085814d08be43e097567
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-12-01 13:20:40 -0600

    creating grid for main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 export default function Main() {
     return (
         <div className="grid">
-            <h1>Make Remote work</h1>
-            <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch producitvity soar.</p>
-            <button type="button">Learn More</button>
+            <h1 className="main--h1">Make Remote work</h1>
+            <p className="main--p">Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch producitvity soar.</p>
+            <button className="main--button" type="button">Learn More</button>
+            <img src="/images/desktop.png" alt="guy with laptop" />
         </div>
     )
 }
