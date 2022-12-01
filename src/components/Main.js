0e3dcc10c81077e827ba0919f8678a7719fb56f6
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-12-01 12:55:42 -0600

    Added nav bar to static page

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ export default function Main() {
         <div className="grid">
             <h1>Make Remote work</h1>
             <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch producitvity soar.</p>
+            <button type="button">Learn More</button>
         </div>
     )
 }
