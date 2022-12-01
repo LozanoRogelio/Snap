e79ae15dbdb6b4110b7c5997183f3ba7e48874e5
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-12-01 12:52:38 -0600

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+export default function Main() {
+    return (
+        <div className="grid">
+            <h1>Make Remote work</h1>
+            <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch producitvity soar.</p>
+        </div>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+
+export default function Nav() {
+    
+    return (
+        <nav>
+        <h1 className="nav--h1" >snap</h1>
+        <h2 className="nav--h2" >Features</h2>
+        <h2 className="nav--h2" >Company</h2>
+        <h2 className="nav--h2" >Careers</h2>
+        <h2 className="nav--h2" id="about" >About</h2>
+        <h2 className="nav--h2" >Login</h2>
+        <button className="nav--button"type="button">Register</button>
+        </nav>
+    )
+}
